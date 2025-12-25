85cde2fcd7e9801cc7850c8b323a7ff9f3a3467a
mallan67 <mayad67@gmail.com>
2025-12-25 03:42:34 -0500

    Delete app/layout.tsx

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import "../src/app/globals.css"
-import RootLayout from "../src/app/layout"
-
-export { metadata, viewport } from "../src/app/layout"
-export default RootLayout
-
-export const metadata = {
-      generator: 'v0.app'
-    };
