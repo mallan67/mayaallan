0c877a51b3ce0a191b517839329b5c332ce28163
mallan67 <mayad67@gmail.com>
2025-12-26 02:13:13 -0500

    Create slugify.ts

---
+++ lib/slugify.ts
@@ -0,0 +1,8 @@
+// lib/slugify.ts
+export function slugify(input: string): string {
+  return input
+    .trim()
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+}
