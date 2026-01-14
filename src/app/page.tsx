c90c8ff6434a6cf7ab6ad8d6686a746a0d46681f
mallan67 <mayad67@gmail.com>
2026-01-14 05:13:58 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image"
 import Link from "next/link"
-import { prisma } from "@/lib/prisma"
 import type { Metadata } from "next"
 import { NewsletterSection } from "@/components/NewsletterSection"
 
